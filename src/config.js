c3a5968c6fea4ba145bc2f02d7f7d8efb037dcb5
Zebreus <lennarteichhorn@googlemail.com>
2024-02-10 14:29:41 +0100

    Change bundler to vite

--- src/config.js
+++ src/config.js
@@ -1,9 +1,7 @@
 const infra = "ofborg.org";
 
-const WELL_KNOWN = `https://logs.${infra}/logs`;
-const SOCK = `wss://messages.ofborg.org:15673/ws/`;
-const SOCK_VHOST = `ofborg`;
-const AUTH = "ofborg-logviewer";
-const MAX_LINES = 25000;
-
-export {SOCK, SOCK_VHOST, AUTH, MAX_LINES, WELL_KNOWN};
+export const WELL_KNOWN = `https://logs.${infra}/logs`;
+export const SOCK = `wss://messages.ofborg.org:15673/ws/`;
+export const SOCK_VHOST = `ofborg`;
+export const AUTH = "ofborg-logviewer";
+export const MAX_LINES = 25000;
